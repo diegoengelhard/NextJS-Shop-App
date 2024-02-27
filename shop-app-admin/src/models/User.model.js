55fcb1edfc7e1c8130981d170581fb020ad709e9
Diego Engelhard <00356119@uca.edu.sv>
2024-02-27 16:31:25 -0600

    feat(admin): declare user model

---
+++ shop-app-admin/src/models/User.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter a name"],
+        },
+        email: {
+            type: String,
+            required: [true, "Please enter an email"],
+            unique: [true, 'Email must be unique'],
+            trim: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+        },
+        password: {
+            type: String,
+            required: [true, "Please enter a password"],
+        },
+        isAdmin: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
