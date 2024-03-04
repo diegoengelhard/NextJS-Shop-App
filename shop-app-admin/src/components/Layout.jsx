5f02c63b3a1441ddd13671f7ddddbab3b49f8e31
Diego Engelhard <00356119@uca.edu.sv>
2024-03-03 22:08:33 -0600

    style(admin): logo & navbar styles

--- shop-app-admin/src/components/Layout.jsx
+++ shop-app-admin/src/components/Layout.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { signIn, signOut, useSession } from "next-auth/react";
-import { toast } from 'react-toastify';
+import Logo from './Logo';
 
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
+    const [showNav, setShowNav] = useState(false);
 
     return (
         <>
             <div className='bg-purple-500 min-h-screen flex'>
-                <Navbar />
+                <Navbar showNavbar={showNav} />
                 <section className='bg-white flex-grow my-2 mr-2 rounded-lg p-4'>
                     {children}
                 </section>
