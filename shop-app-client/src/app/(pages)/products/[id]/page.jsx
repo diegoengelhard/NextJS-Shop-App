03aa6433064fd2c60c4a179951641f659c8b48d8
Diego Engelhard <00356119@uca.edu.sv>
2024-04-01 17:02:30 -0600

    feat(client): single product page

--- shop-app-client/src/app/(pages)/products/[id]/page.jsx
+++ shop-app-client/src/app/(pages)/products/[id]/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useState, useContext } from 'react'
 import { useParams } from "next/navigation";
+import { useSession } from "next-auth/react";
 import axios from 'axios';
 
 import Header from '@/components/Header/Header';
@@ -9,10 +10,23 @@ import Spinner from '@/components/Spinner';
 import Button from '@/components/Buttons/Button';
 import CartIcon from '@/components/Icons/CartIcon';
 import { CartContext } from '@/components/CartContext';
+import { toast } from 'react-toastify';
 
-import { ColWrapper, PriceRow, Price, WhiteBox, Title } from './styles';
+import {
+    Container,
+    WhiteBox,
+    ProductName,
+    PriceDiscount,
+    RoundBlackBtn,
+    SliderContainer,
+    Flex,
+    Carousel,
+    ProductInfo,
+    ProductImage,
+} from './styles';
 
 const SingleProductPage = () => {
+    const { data: session } = useSession(); // get session from next-auth
     const { id } = useParams(); // get params from url
     const { addToCart } = useContext(CartContext); // get addToCart function from context
 
@@ -40,13 +54,62 @@ const SingleProductPage = () => {
 
     console.log(product);
 
+    // Add the product to the cart
+    const handleAddToCart = () => {
+        if (session) {
+            addToCart(product);
+        } else {
+            return toast.warn('Please sign in to add to cart');
+        }
+    }
+
     return (
         <>
             <Header />
             <Center>
                 {loading ? <Spinner /> : (
                     <Center>
-                        <p>Here Product Page</p>
+                        <Container>
+                            <Flex>
+                                {/* Product Images */}
+                                <div className="col-md-6">
+                                    <SliderContainer>
+                                        <div id="slider" className="owl-carousel product-slider">
+                                            <div id="slider" className="owl-carousel product-slider">
+                                                <WhiteBox>
+                                                    <ProductImage src={product?.photos?.[0]} alt="Slider Image 1" />
+                                                </WhiteBox>
+                                            </div>
+                                        </div>
+                                    </SliderContainer>
+                                </div>
+
+                                {/* Product Info */}
+                                <div className="col-md-6">
+                                    <div className="product-dtl">
+                                        <div className="product-info">
+                                            <ProductName>{product?.title}</ProductName>
+                                            <PriceDiscount>
+                                                <span>${product?.price}</span>
+                                            </PriceDiscount>
+                                        </div>
+                                        <ProductInfo>
+                                            {product?.description}
+                                        </ProductInfo>
+                                        <div className="product-count">
+                                            <RoundBlackBtn
+                                                onClick={handleAddToCart}
+                                            >
+                                                Add to Cart
+                                            </RoundBlackBtn>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Flex>
+
+                            {/* Product Images Carousel */}
+                            <Carousel></Carousel>
+                        </Container>
                     </Center>
                 )}
             </Center>
--- shop-app-client/src/app/(pages)/products/[id]/styles.js
+++ shop-app-client/src/app/(pages)/products/[id]/styles.js
@@ -24,9 +24,108 @@ export const WhiteBox = styled.div`
   background-color: #fff;
   border-radius: 10px;
   padding: 30px;
+  height: 50vh;
+  overflow: hidden;
+`;
+
+export const ProductImage = styled.img`
+  max-width: 100%;
+  height: 100%;
 `;
 
 export const Title = styled.h1`
   font-size: 1.5em;
 `;
 
+export const Container = styled.div`
+    padding: 40px 0;
+    font-family: 'Poppins', sans-serif;
+`;
+
+export const Heading = styled.h2`
+    font-size: 32px;
+    font-weight: 500;
+    padding-top: 10px;
+    padding-bottom: 15px;
+`;
+
+export const ProductName = styled.div`
+    font-size: 22px;
+    font-weight: 500;
+    line-height: 22px;
+    margin-bottom: 4px;
+`;
+
+export const PriceDiscount = styled.div`
+    font-size: 22px;
+    font-weight: 400;
+    padding: 10px 0;
+    clear: both;
+
+    span.line-through {
+        text-decoration: line-through;
+        margin-left: 10px;
+        font-size: 14px;
+        vertical-align: middle;
+        color: #a5a5a5;
+    }
+`;
+
+export const FormControl = styled.div`
+    font-size: 15px;
+`;
+
+export const RoundBlackBtn = styled.a`
+    border-radius: 4px;
+    background: #212529;
+    color: #fff;
+    padding: 7px 45px;
+    display: inline-block;
+    margin-top: 20px;
+    border: solid 2px #212529;
+    transition: all 0.5s ease-in-out 0s;
+
+    &:hover,
+    &:focus {
+        background: transparent;
+        color: #212529;
+        text-decoration: none;
+    }
+`;
+
+export const SliderContainer = styled.div`
+    .owl-carousel {
+        // Add your slider styles here
+    }
+
+    .owl-carousel .item img {
+        height: 300px; // Adjust the height as per your requirement
+        width: auto;
+        display: block;
+        margin: 0 auto;
+    }
+`;
+
+export const ThumbnailContainer = styled.div`
+    .owl-carousel {
+        // Add your thumbnail slider styles here
+    }
+
+    .owl-carousel .item img {
+        height: 100px; // Adjust the height as per your requirement
+        width: auto;
+        display: flex;
+        margin: 0 auto;
+    }
+`;
+
+export const ProductInfo = styled.div`
+    font-weight: 400;
+`;  
+
+export const Flex = styled.div`
+    display: flex;
+    gap: 20px;
+`;
+
+export const Carousel = styled.div``;
